9e44bf0bc81fd751f40af728b1636f71fd930942
Hyman <1392841128@qq.com>
2018-09-19 22:32:49 +0900

    2018.09.19 外观模式 Appearance Pattern

---
+++ 外观模式 Appearance Pattern/源码/java/jikexueyuan/facademode/hometheater/Car.java
@@ -0,0 +1,21 @@
+package com.java.jikexueyuan.facademode.hometheater;
+
+public class Car{
+	Engine engine;
+	public Car()
+	{
+		//初始化发动机
+	}
+	
+	public void start(Key mKey)
+	{
+		Doors doors=new Doors();
+		boolean authorized=mKey.turns();
+		if(authorized)
+		{
+			engine.start();
+			doors.lock();
+		}
+	}
+
+}
