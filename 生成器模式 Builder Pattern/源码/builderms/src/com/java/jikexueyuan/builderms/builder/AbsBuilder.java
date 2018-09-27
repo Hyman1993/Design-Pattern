579b4dd2524bccf68a167b5319ec5807d9153816
Hyman <1392841128@qq.com>
2018-09-27 22:09:09 +0900

    2018.09.27 生成器模式 Builder Pattern

---
+++ 生成器模式 Builder Pattern/源码/builderms/src/com/java/jikexueyuan/builderms/builder/AbsBuilder.java
@@ -0,0 +1,29 @@
+package com.java.jikexueyuan.builderms.builder;
+
+import com.java.jikexueyuan.builderms.vacation.Vacation;
+
+public abstract class AbsBuilder {
+
+	public Vacation mVacation;
+
+	public AbsBuilder(String std) {
+		mVacation = new Vacation(std);
+	}
+
+	public abstract void buildvacation();
+
+	public abstract void buildDay(int i);
+
+	public abstract void addHotel(String hotel);
+
+	public abstract void addTicket(String ticket);
+
+	public abstract void addEvent(String tvent);
+
+	public Vacation getVacation() {
+
+		return mVacation;
+
+	}
+
+}
