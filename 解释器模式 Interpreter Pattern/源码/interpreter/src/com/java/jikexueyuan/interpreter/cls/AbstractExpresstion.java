0d4630158d2c23a940c25ec04ffb945716fce381
Hyman <1392841128@qq.com>
2018-09-29 15:28:03 +0900

    2018.09.29 解释器模式 Interpreter Pattern

---
+++ 解释器模式 Interpreter Pattern/源码/interpreter/src/com/java/jikexueyuan/interpreter/cls/AbstractExpresstion.java
@@ -0,0 +1,7 @@
+package com.java.jikexueyuan.interpreter.cls;
+
+import java.util.HashMap;
+
+public abstract class AbstractExpresstion {
+	public abstract Float interpreter(HashMap<String, Float> var);
+}
